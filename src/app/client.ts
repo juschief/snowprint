1fa61460dc75727ff1e5b2a7bacc5b29893da086
Chief <63847806+juschief@users.noreply.github.com>
2025-01-02 03:31:05 -0500

    minor bugs

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,5 +2,5 @@ import { createThirdwebClient } from "thirdweb";
 
 // Create a client
 export const client = createThirdwebClient({
-  clientId: '4a25552d9112bf4d088dec97f1fa90e5' || "", // Get this from thirdweb dashboard
+  clientId: '4a25552d9112bf4d088dec97f1fa90e5', // Get this from thirdweb dashboard
 });
